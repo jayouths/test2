39db3f474e11ad009232f381aadc481fbf18ed46
chenzhoulin <chenzhoulin@kuaishou.com>
2025-11-27 18:21:01 +0800

    Add Dog

---
+++ src/main/java/Dog.java
@@ -0,0 +1,9 @@
+/**
+ * @author chenzhoulin <chenzhoulin@kuaishou.com>
+ * Created on 2025-11-27
+ */
+public class Dog {
+    private String name;
+    private int age;
+    public Dog(String name, int age) {}
+}
