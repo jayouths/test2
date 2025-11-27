53e80e87866de93593bbf60b6de32db0d4aef6cc
chenzhoulin <chenzhoulin@kuaishou.com>
2025-11-27 18:07:51 +0800

    initial commit

---
+++ src/main/java/testClass.java
@@ -0,0 +1,9 @@
+/**
+ * @author chenzhoulin <chenzhoulin@kuaishou.com>
+ * Created on 2025-11-27
+ */
+public class testClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
