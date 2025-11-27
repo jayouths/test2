10a653b2c973e30cf6ed1afbd40506e1f83124a2
chenzhoulin <chenzhoulin@kuaishou.com>
2025-11-27 18:43:08 +0800

    Add Snake

---
+++ src/main/java/Snake.java
@@ -0,0 +1,11 @@
+/**
+ * @author chenzhoulin <chenzhoulin@kuaishou.com>
+ * Created on 2025-11-27
+ */
+public class Snake {
+    private String name;
+    private int age;
+    public Snake(String name, int age) {
+        this.name = name;
+    }
+}
